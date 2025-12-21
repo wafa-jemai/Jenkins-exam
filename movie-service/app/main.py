fed03967871e598559319027061c9b574874f65d
wafa-jemai <wafa.jemai@hotmail.com>
2025-12-21 16:09:32 +0000

    Fix uvloop error for movie-service

--- movie-service/app/main.py
+++ movie-service/app/main.py
@@ -1,8 +1,13 @@
 from fastapi import FastAPI
 from app.api.movies import movies
 from app.api.db import metadata, database, engine
+import asyncio
+import uvicorn
 
 metadata.create_all(engine)
+
+# Fix uvloop bug: force default event loop policy
+asyncio.set_event_loop_policy(asyncio.DefaultEventLoopPolicy())
 
 app = FastAPI(openapi_url="/api/v1/movies/openapi.json", docs_url="/api/v1/movies/docs")
 
@@ -14,5 +19,14 @@ async def startup():
 async def shutdown():
     await database.disconnect()
 
-
 app.include_router(movies, prefix='/api/v1/movies', tags=['movies'])
+
+if __name__ == "__main__":
+    uvicorn.run(
+        "app.main:app",
+        host="0.0.0.0",
+        port=8000,
+        loop="asyncio",
+        reload=False
+    )
+

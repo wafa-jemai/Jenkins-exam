31c36d1b6197c147fdd902208680dfba9e592b86
wafa-jemai <wafa.jemai@hotmail.com>
2025-12-25 22:17:55 +0000

    Fix DATABASE_URL + Helm stability

--- movie-service/app/api/db.py
+++ movie-service/app/api/db.py
@@ -1,12 +1,17 @@
 import os
-from sqlalchemy import Column, Integer, String, Table, MetaData
-from sqlalchemy.ext.asyncio import create_async_engine
+from sqlalchemy import Column, Integer, String, Table, MetaData, create_engine
+from sqlalchemy.dialects.sqlite import JSON
 from databases import Database
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 if not DATABASE_URL:
     raise Exception("DATABASE_URL must be set")
+
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={"check_same_thread": False} if DATABASE_URL.startswith("sqlite") else {}
+)
 
 metadata = MetaData()
 
@@ -16,8 +21,9 @@ movies = Table(
     Column("id", Integer, primary_key=True),
     Column("name", String(50)),
     Column("plot", String(250)),
+    Column("genres", JSON),
+    Column("casts_id", JSON),
 )
 
 database = Database(DATABASE_URL)
-engine = create_async_engine(DATABASE_URL)
 

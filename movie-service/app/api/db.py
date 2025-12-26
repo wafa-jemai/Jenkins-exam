2b8b03149df23bd3a4d7681593911038fa1fb635
wafa-jemai <wafa.jemai@hotmail.com>
2025-12-26 22:16:02 +0000

    modif

--- movie-service/app/api/db.py
+++ movie-service/app/api/db.py
@@ -1,6 +1,5 @@
 import os
-from sqlalchemy import Column, Integer, String, Table, MetaData, create_engine
-from sqlalchemy.dialects.sqlite import JSON
+from sqlalchemy import create_engine, MetaData
 from databases import Database
 
 DATABASE_URL = os.getenv("DATABASE_URL")
@@ -10,20 +9,10 @@ if not DATABASE_URL:
 
 engine = create_engine(
     DATABASE_URL,
-    connect_args={"check_same_thread": False} if DATABASE_URL.startswith("sqlite") else {}
+    connect_args={"check_same_thread": False}
+    if DATABASE_URL.startswith("sqlite") else {}
 )
 
 metadata = MetaData()
-
-movies = Table(
-    "movies",
-    metadata,
-    Column("id", Integer, primary_key=True),
-    Column("name", String(50)),
-    Column("plot", String(250)),
-    Column("genres", JSON),
-    Column("casts_id", JSON),
-)
-
 database = Database(DATABASE_URL)
 

bace7e274369ffd648fc2b524107130da499463b
wafa-jemai <wafa.jemai@hotmail.com>
2025-12-27 22:27:25 +0000

    Fix services + helm + env

--- movie-service/app/api/db.py
+++ movie-service/app/api/db.py
@@ -2,15 +2,11 @@ import os
 from sqlalchemy import create_engine, MetaData
 from databases import Database
 
-DATABASE_URL = os.getenv("DATABASE_URL")
-
-if not DATABASE_URL:
-    raise Exception("DATABASE_URL must be set")
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./app/api/db.sqlite3")
 
 engine = create_engine(
     DATABASE_URL,
-    connect_args={"check_same_thread": False}
-    if DATABASE_URL.startswith("sqlite") else {}
+    connect_args={"check_same_thread": False} if DATABASE_URL.startswith("sqlite") else {}
 )
 
 metadata = MetaData()
